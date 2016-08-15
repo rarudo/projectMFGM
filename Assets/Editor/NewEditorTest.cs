63ff651081619d046275f812164f165c8b13ac71
rarudo <rarudonet@gmail.com>
2016-08-15 15:19:48 +0900

    FirstCommit

---
+++ Assets/Editor/NewEditorTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class NewEditorTest {
+
+	[Test]
+	public void EditorTest()
+	{
+		//Arrange
+		var gameObject = new GameObject();
+
+		//Act
+		//Try to rename the GameObject
+		var newGameObjectName = "My game object";
+		gameObject.name = newGameObjectName;
+
+		//Assert
+		//The object has a new name
+		Assert.AreEqual(newGameObjectName, gameObject.name);
+	}
+
+	[Test]
+	public void 日本語テスト(){}
+
+    [Test]
+    public void 作った弾丸オブジェクトの名前チェック(){
+        CameraController tes = new CameraController();
+        GameObject go = tes.makeBullet();
+        Assert.AreEqual("Bullet",go.name,"Bulletって言うオブジェクトが生成されてるはず");
+    }
+}
---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            makeBullet();
+        }
+    }
+    public GameObject makeBullet() {
+        GameObject bullet = GameObject.CreatePrimitive(PrimitiveType.Sphere); //球体の作成
+        bullet.name = "Bullet";
+        return bullet; 
+    }
+}
