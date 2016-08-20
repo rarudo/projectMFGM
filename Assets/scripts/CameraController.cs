47d729a3812cf665f97d3196b6071a8debd14247
rarudo <rarudonet@gmail.com>
2016-08-20 15:30:15 +0900

    modify bullet spawn

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -2,31 +2,34 @@
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
+    public string prefabFolder_ = "Prefabs";
+    public string prefabName_ = "BulletBase";
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+    }
 
-
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
         if (Input.GetMouseButtonDown(0))
         {
             makeBullet();
         }
     }
     public GameObject makeBullet() {
-        GameObject bullet = GameObject.CreatePrimitive(PrimitiveType.Sphere); //球体の作成
+        //弾丸を作成プレハブから持ってくる
+        string prefabName = prefabFolder_ + "/" + prefabName_;
+        GameObject bullet = Instantiate(Resources.Load(prefabName)) as GameObject;
         bullet.name = "Bullet";
         bullet = setLocationPlayer(bullet);
         return bullet; 
     }
+
     //オブジェクトを出現させる座標をPlayerの座標にする
     public GameObject setLocationPlayer(GameObject gObject) {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
-        gObject.transform.position = player.transform.position;
+        Vector3 playerPosition = player.transform.position;
+        gObject.transform.position = playerPosition + player.transform.forward;
         return gObject;
     }
 }
